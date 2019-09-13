75b64b516e9f666c629ea49f16baaba0f97854e9
jgu7man <46007655+jgu7man@users.noreply.github.com>
2019-09-13 10:48:29 -0500

    cambios para la privacidad

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'grupoVetsa'`, () => {
+  it(`should have as title 'SQLadmin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('grupoVetsa');
+    expect(app.title).toEqual('SQLadmin');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to grupoVetsa!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to SQLadmin!');
   });
 });
